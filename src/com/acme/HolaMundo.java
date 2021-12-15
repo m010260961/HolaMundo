9ec3431610c4dbd1e6aaa21414506d5c373b1437
deaves <deaves1992@gmail.com>
2021-12-15 01:26:27 -0600

    Se integro la clase Mensaje

--- src/com/acme/HolaMundo.java
+++ src/com/acme/HolaMundo.java
@@ -3,6 +3,9 @@ package com.acme;
 public class HolaMundo {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        Mensaje msg = new Mensaje();
+        String nombre = msg.solictaDatos( "Escribe tu nombre: ");
+        msg.saludar(nombre);
     }
+
 }
