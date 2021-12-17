5ca53d50aec2428ac6162f5a77364eedb06a6022
Ricky Deaves Bautista López <90150555+m010260961@users.noreply.github.com>
2021-12-16 19:13:56 -0600

    Cometerio de codigo inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -4,7 +4,8 @@ public record PersonaRegistro (String nombre, String apellidos) {
 
     @Override
     public String toString() {
-        System.out.println("Esta instrucción es inservible");
+        //Alejandro comente la linea de codigo que considero se puede elimin
+         //System.out.println("Esta instruccuon es inservible")
         return "%s %s". formatted(nombre, apellidos);
     }
 }
