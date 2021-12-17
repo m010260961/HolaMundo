3384cbe7a3b7b38cc15f2421a9202b05cfb087a2
deaves <deaves1992@gmail.com>
2021-12-16 20:20:41 -0600

    Se remplaza clase Persona por registro Persona

--- src/com/acme/Persona.java
+++ src/com/acme/Persona.java
@@ -1,33 +1,9 @@
 package com.acme;
 
-public class Persona {
-
-    private String nombre,apellidos;
-
-    public Persona(String nombre, String apellidos) {
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellido) {
-        this.apellidos = apellido;
-    }
+public record Persona(String nombre, String apellidos) {
 
     @Override
     public String toString() {
-        return "%s %s".formatted(nombre,apellidos);
+        return "%s %s". formatted(nombre, apellidos);
     }
 }
-
